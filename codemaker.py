2a0d9abfd62363f4bd0b2ffb3318eabeb0d49c37
pgnj99 <pgiordanonj@gmail.com>
2024-08-28 21:46:56 -0400

    Filled out PuzzleGame class, added make_marks as base class

--- codemaker.py
+++ codemaker.py
@@ -3,10 +3,14 @@ import random
 class Codemaker:
 
     colors_full = ['B', 'R', 'G', 'Y', 'P', 'C', 'W']
+    mark_correct = "●"
+    mark_close = "◌"
+    mark_missed = "·"
 
     def __init__(self, total, given):
         self.colors = Codemaker.colors_full[:given]
         self.code = self.make_code(total)
+        self.marks = []
 
     def make_code(self, total):
         code = []
@@ -14,25 +18,7 @@ class Codemaker:
             code.append(random.choice(self.colors))
         return code
 
-    def list_colors(self):
-        print("Available colors: ")
-        for color in self.colors:
-            print(color, end="  ")
-        print()
-
-
-class ClassicGame(Codemaker):
-    def __init__(self, total, given):
-        super().__init__(total, given)
-        self.table = []
-        self.marks = []
-
-        self.guesses = 10
-
-        for i in range(self.guesses):
-            self.table.append(["·"] * total)
-
-    def check(self, guess):
+    def make_marks(self, guess):
         correct = 0
         close = 0
         checked = []
@@ -48,12 +34,33 @@ class ClassicGame(Codemaker):
                         checked.append(j)
                         break
         
+        missed = len(self.code) - correct - close
+        
+        return self.mark_correct * correct + self.mark_close * close + self.mark_missed * missed
+
+    def list_colors(self):
+        print("Available colors: ")
+        for color in self.colors:
+            print(color, end="  ")
+        print()
+
+
+class ClassicGame(Codemaker):
+    def __init__(self, total, given):
+        super().__init__(total, given)
+        self.table = []
+        self.guesses = 10
+
+        for i in range(self.guesses):
+            self.table.append(["·"] * total)
+
+    def check(self, guess):
         self.update_table(guess)
 
-        missed = len(self.code) - correct - close
-        self.marks.append("●" * correct + "◌" * close + "·" * missed)
+        mark = self.make_marks(guess)
+        self.marks.append(mark)
 
-        if correct == len(self.code):
+        if guess == self.code:
             return True
         else:
             return False
@@ -88,3 +95,12 @@ class PuzzleGame(Codemaker):
             clue = self.make_code(total)
             if clue != self.code and clue not in self.clues:
                 self.clues.append(clue)
+        
+        for clue in self.clues:
+            self.marks.append(self.make_marks(clue))
+
+    def display_clues(self):
+        for i in range(len(self.clues)):
+            for peg in self.clues[i]:
+                print(peg, end="  ")
+            print(self.marks[i])
