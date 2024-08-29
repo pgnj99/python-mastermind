d4dd71991ea0133ef87766e134cca8921b70ab70
pgnj99 <pgiordanonj@gmail.com>
2024-08-28 20:17:25 -0400

    Completed classic game loop, added display functions

--- codemaker.py
+++ codemaker.py
@@ -7,8 +7,16 @@ class Codemaker:
     def __init__(self, total, given):
         self.colors = Codemaker.colors_full[:given]
         self.code = []
+        self.table = []
+        self.marks = []
+
+        self.guesses = 10
+
         for i in range(total):
             self.code.append(random.choice(self.colors))
+
+        for i in range(self.guesses):
+            self.table.append(["·"] * total)
 
     def check(self, guess):
         correct = 0
@@ -22,14 +30,32 @@ class Codemaker:
             elif guess[i] in self.code and i not in checked:
                 close += 1
         
+        self.update_table(guess)
+
         missed = len(self.code) - correct - close
-        print("●" * correct + "◌" * checked + "·" * missed)
+        self.marks.append("●" * correct + "◌" * checked + "·" * missed)
 
         if correct == len(self.code):
             return True
         else:
             return False
 
+    def update_table(self, guess):
+        for row in self.table:
+            if "·" in row:
+                row = guess
+                break
+    
+    def display_table(self):
+        for i in range(self.guesses - 1, -1, -1):
+            for peg in self.table[i]:
+                print(peg, end="  ")
+                if "·" not in self.table[i]:
+                    print(self.marks[i])
+    
+    def display_code(self):
+        for peg in self.code:
+            print(peg, end="  ")
 
     def list_colors(self):
         print("Available colors: ")
--- play.py
+++ play.py
@@ -2,19 +2,30 @@ from codemaker import *
 
 def classic(total, given):
     code = Codemaker(total, given)
-    code.list_colors()
-    
-    guess = input("Make your guess here: ")
-    guesslist = []
+    chances = code.guesses
+    guesses = 0
 
     while True:
-        for peg in guess:
-            if peg.isalpha():
-                guesslist.append(peg.upper())
-        
-        if len(guesslist) != len(code.code):
-            guess = input("Invalid guess. Try again: ")
-        else:
+        code.display_table()
+        if chances == guesses:
+            print("Sorry, you've run out of guesses!")
+            print("The correct code:")
+            code.display_code()
             break
+        print("You have " + str(chances - guesses) + " guesses remaining.")
+        code.list_colors()
 
-    code.check(guess)
+        guess = input("Make your guess here: ")
+        guesslist = []
+
+        while True:
+            for peg in guess:
+                if peg.isalpha():
+                    guesslist.append(peg.upper())
+            
+            if len(guesslist) != len(code.code):
+                guess = input("Invalid guess. Try again: ")
+            else:
+                break
+
+        code.check(guesslist)
