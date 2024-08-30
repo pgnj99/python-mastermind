4d76c8a54c713287f8352bd3cbbf49389f6208b8
pgnj99 <pgiordanonj@gmail.com>
2024-08-30 00:21:21 -0400

    Reworked Codemaker class to support settings and colored text

--- codemaker.py
+++ codemaker.py
@@ -2,15 +2,17 @@ import random
 
 class Codemaker:
 
-    colors_full = ['B', 'R', 'G', 'Y', 'P', 'C', 'W']
-    mark_correct = "●"
-    mark_close = "◌"
-    mark_missed = "·"
+    colors_full = {'Blue': 34, 'Red': 31, 'Green': 32, 'Yellow': 93, 'Pink': 95, 'Cyan': 36, 'White': 47}
 
-    def __init__(self, total, given):
-        self.colors = Codemaker.colors_full[:given]
+    def __init__(self, total, given, settings):
+        self.colors = [color for color in Codemaker.colors_full[:given]]
         self.code = self.make_code(total)
         self.marks = []
+
+        self.settings = settings
+        self.mark_correct = self.settings[1][0]
+        self.mark_close = self.settings[1][1]
+        self.mark_missed = self.settings[1][2]
 
     def make_code(self, total):
         code = []
@@ -41,13 +43,26 @@ class Codemaker:
     def list_colors(self):
         print("Available colors: ")
         for color in self.colors:
-            print(color, end="  ")
+            print(self.peg_color(color), end="  ")
         print()
+
+    def peg_color(self, text, settings = 0):
+        if settings == 0:
+            settings = self.settings
+            text = text[0]
+
+        if settings[0] == 0:
+            return text
+        elif settings[0] == 1:
+            return '\033[' + str(self.colors_full[text]) + 'm' + text + '\033[0m'
+        elif settings[0] == 2:
+            return '\033[30m\033[' + str(self.colors_full[text] + 10) + 'm' + text + '\033[0m'
+
 
 
 class ClassicGame(Codemaker):
-    def __init__(self, total, given):
-        super().__init__(total, given)
+    def __init__(self, total, given, settings):
+        super().__init__(total, given, settings)
         self.table = []
         self.guesses = 10
 
@@ -74,7 +89,7 @@ class ClassicGame(Codemaker):
     def display_table(self):
         for i in range(self.guesses - 1, -1, -1):
             for peg in self.table[i]:
-                print(peg, end="  ")
+                print(self.peg_color(peg), end="  ")
             if "·" not in self.table[i]:
                 print(self.marks[i])
             else:
@@ -82,13 +97,13 @@ class ClassicGame(Codemaker):
     
     def display_code(self):
         for peg in self.code:
-            print(peg, end="  ")
+            print(self.peg_color(peg), end="  ")
         print()
 
 
 class PuzzleGame(Codemaker):
-    def __init__(self, total, given, count):
-        super().__init__(total, given)
+    def __init__(self, total, given, count, settings):
+        super().__init__(total, given, settings)
         self.clues = []
 
         while len(self.clues) < count:
@@ -102,7 +117,7 @@ class PuzzleGame(Codemaker):
     def display_clues(self):
         for i in range(len(self.clues)):
             for peg in self.clues[i]:
-                print(peg, end="  ")
+                print(self.peg_color(peg), end="  ")
             print(self.marks[i])
 
     def compare(self, guess):
